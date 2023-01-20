e54c7abffb64500dace1e6fb24618ed47b5a809e
KristiSeraj <serajkristi@gmail.com>
2023-01-20 13:44:09 +0100

    wikipedia parsed

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -6,6 +6,15 @@ function createElement(data) {
 
 function queryWikipedia(callback) {
     let content = new XMLHttpRequest();
-
     content.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*")
+    content.onload = function() {
+        if (content.status == 200) {
+        		let obj = JSON.parse(content.responseText);
+            callback(obj["query"]["pages"]["21721040"]["extract"]);
+        }
+    }
+    content.send();
 }
+
+
+queryWikipedia(createElement)
